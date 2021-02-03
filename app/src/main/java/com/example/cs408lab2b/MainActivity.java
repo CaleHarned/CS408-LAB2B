ca94e0d6b3861ed1f8c9efff93d2cc3835bf6b29
CaleHarned <caleharned@gmail.com>
2021-02-03 13:54:28 -0600

    Finished commit

--- app/src/main/java/com/example/cs408lab2b/MainActivity.java
+++ app/src/main/java/com/example/cs408lab2b/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.cs408lab2b;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] helpArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        helpArray = getResources().getStringArray(R.array.help);;
+    }
+    public void onClickHelpButton(View v) {
+        Random rand = new Random();
+        int bound= 18;
+        int randomHelp= rand.nextInt(bound);
+        TextView a = (TextView) findViewById(R.id.outputText);
+        a.setText(helpArray[randomHelp]);
+
     }
 }
